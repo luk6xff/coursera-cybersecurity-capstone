851eeedd1f5507cdf39a96e05007e28e0fbe951c
luk6xff <lukasz.uszko@gmail.com>
2021-04-19 10:01:54 +0200

    Login and register layout

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # coursera-cybersecurity-capstone
 Solution for [Coursera Cybersecurity Capstone Project](https://www.coursera.org/learn/cyber-security-capstone)
+
+
+## Setup environment
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Create project and apps
+```
+django-admin startproject chat_project
+cd chat_project
+django-admin startapp account
+```
+
+## Run the app
+
+### Build
+```
+cd chat_project
+python manage.py makemigrations
+python manage.py migrate
+```
+
+### Run a development server
+```
+python manage.py runserver
+```
+Go to: `http://127.0.0.1:8000/`
+
+### Run a production server
---
+++ chat_project/account/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+
+class Message(models.Model):
+     sender = models.ForeignKey(Profile, related_name="sender", null=True)
+     reciever = models.ForeignKey(Profile, related_name="receiver", null=True)
+     message_content = models.CharField(max_length=250, null=True, blank=True)
+     creation_time = models.DateTimeField(null=True, auto_now_add=True)
+
+     def __str__(self):
+         return self.message_content
